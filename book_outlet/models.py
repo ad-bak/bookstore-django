394a0ca7274997aae15adb93e43fd3c3282cf6ae
Adnan <adbak.dev@gmail.com>
2023-11-29 23:18:55 +0100

    lc

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,9 +4,36 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state} {self.zip}"
+
+    class Meta:
+        verbose_name_plural = "Addresses"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True, blank=True
+    )
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -26,6 +53,7 @@ class Book(models.Model):
     )
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
